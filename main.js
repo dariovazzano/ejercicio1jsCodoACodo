c5f7c68a517da76d8774628b4339cbc0107b4704
dariovazzano <98060971+dariovazzano@users.noreply.github.com>
2022-05-08 23:49:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let primera;
+let segunda;
+let sumaTotal;
+
+primera= parseInt(prompt("Ingrese el valor numerico 40"));
+segunda= parseInt(prompt("Ingrese el valor numerico 16"));
+sumaTotal = primera + segunda;
+
+document.getElementById('input').textContent = sumaTotal;
